f29237c87ab3963d57edc151c4f9cbc17a99e392
@jamgorilla <jamesmurphy507@hotmail.com>
2017-04-30 19:31:12 +0100

    new branch commit

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -1,10 +1,10 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
- const TableModel = require('./table-model');
+  const TableModel = require('./table-model');
  const TableView = require('./table-view');
 
  const model = new TableModel();
  const tableView = new TableView(model);
- tableView.init()
+ tableView.init();
 },{"./table-model":4,"./table-view":5}],2:[function(require,module,exports){
 const getRange = function(fromNum, toNum) {
 	return Array.from({ length: toNum - fromNum + 1 },
